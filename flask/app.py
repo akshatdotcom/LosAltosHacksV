b9eb1a7d184cafdbf9e41c055c43c7a3dbf06fbd
Akshat Shah <aksshah2004@gmail.com>
2021-04-25 03:15:50 -0500

    update flask host and port

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from db import pullData, pushData
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
